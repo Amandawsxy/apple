ca95827f26ee261fcbfd2760c8d2cb36b320b550
Amandawsxy <673023661@qq.com>
2016-08-01 08:44:37 +0800

    xg

--- js/index.js
+++ js/index.js
@@ -41,86 +41,9 @@ $(function(){
         $('.xiangxi').toggleClass('shanChu');
         //$('.banner').toggleClass('Shan');
     })
-    //var banner=$('.banner');
-    //var imgs=$('.banner-img');
-    //var Button=$('.Button');
-    //var buttonL=$('.button-left',Button);
-    //var buttonR=$('.button-right',Button);
-    //banner.on("mouseenter",function(){
-    //    clearInterval(t);
-    //    buttonL.css({
-    //        opacity:1,
-    //    })
-    //    buttonR.css({
-    //        opacity:1,
-    //    })
-    //})
-    //banner.on("mouseleave",function(){
-    //    t=setInterval(mover,2000);
-    //    buttonL.css({
-    //        opacity:0,
-    //    })
-    //   buttonR.css({
-    //        opacity:0,
-    //    })
-    //})
-    //var w=imgs.width();
-    //imgs.css({left:w}).eq(0).css({left:0});
-    //var banner=$('.banner');
-    //var lis=$('li',banner);
-    //console.log(lis);
-    //for(var j=0;j<lis.length;j++){
-    //    lis.eq(j).click(function(){
-    //        //alert(1);
-    //        $(this).addClass("active");
-    //        imgs.eq(j).animate({left:0});
-    //    })
-    //
-    //}
-    //lis.click(function(){
-    //    $(this).toggleClass("active");
-    //})
-    //var as=$('a',lis);
-    //as.eq(0).css({background:"#fff",border:"1px solid #1491d0"});
-    //
-    //buttonL.click(function(){
-    //    clearInterval(t);
-    //    mover();
-    //})
-    //
-    //buttonR.click(function(){
-    //    clearInterval(t);
-    //    movel();
-    //})
-    //
-    //
-    //var index=0;//当前的图片
-    //var next=0;//下一张图片
-    //var t=setInterval(mover,2000);
-    //function mover(){
-    //    next++;
-    //    if(next>=4){
-    //        next=0;
-    //    }
-    //    as.css({background:"#999",border:"none"});
-    //    as.eq(next).css({background:"#fff",border:"1px solid #1491d0"});
-    //    imgs.eq(next).css({left:w});
-    //    imgs.eq(index).animate({left:-w},300);
-    //    imgs.eq(next).animate({left:0},300);
-    //    index=next;
-    //}
-    //function movel(){
-    //    next--;
-    //    if(next<0){
-    //        next=3;
-    //    }
-    //    as.css({background:"#999",border:"none"});
-    //    as.eq(next).css({background:"#fff",border:"1px solid #1491d0"});
-    //    imgs.eq(next).css({left:-w});
-    //    imgs.eq(index).animate({left:w},300);
-    //    imgs.eq(next).animate({left:0},300);
-    //    index=next;
-    //}
+
+
+
 
 
     var flag=false;
@@ -219,6 +142,7 @@ $(function(){
 
 //    加号点击
     $('.fa').on('click',function(){
+        //$('.da ul').slideToggle('footer-yingcang1');
         $('.da').toggleClass('gaodu');
         $('.footer-list').toggleClass('one');
         $('.footer-listX ul li a').toggleClass('feiru');
